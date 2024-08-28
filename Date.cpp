1a1e22e940c87f337a7c6cbc9adb66d4d6e9bfce
stefa <stefano.marianelli@stud.unifi.it>
2024-08-28 16:10:55 +0200

    Added to Date class, functions that control if a date is legal

--- Date.cpp
+++ Date.cpp
@@ -4,11 +4,6 @@
 
 #include "Date.h"
 
-//todo: implementare funzioni che verificano giorni, mesi e anni validi
-//todo: implementare funzione che verifica se una data è valida
-//todo: implementare funzione che verifica se un anno è bisestile
-//todo: implementare funzione che "prende" giorni di un mese
-
 Date::Date(int day, int month, int year)
     : day(day), month(month), year(year) {}
 
@@ -31,4 +26,55 @@ int Date::getYear() const {
 }
 void Date::setYear(int year) {
     Date::year = year;
+}
+
+
+bool Date::validYear(int year) {
+    if(year > 2000 && year < 2100)
+        return true;
+    return false;
+}
+
+bool Date::validMonth(int month) {
+    if(month > 0 && month < 13)
+        return true;
+    return false;
+}
+
+bool Date::isLeap(int year) const {
+    if(year % 4 == 0) {
+        if(year % 100 == 0) {
+            if(year % 400 == 0)
+                return true;
+            else
+                return false;
+        }else
+            return true;
+    }
+    return false;
+}
+
+int Date::getDaysOfMonth(int month, int year) const {
+    if(month == 2) {
+        if(isLeap(year))
+            return 29;
+        return 28;
+    }
+    if(month == 4 || month == 6 || month == 9 || month == 11)
+        return 30;
+    return 31;
+}
+
+bool Date::validDay(int day, int month, int year) const {
+    int daysOfMonth = getDaysOfMonth(month, year);
+    if(day > 0 && day <= daysOfMonth)
+        return true;
+    return false;
+}
+
+bool Date::validDate(int day, int month, int year) {
+    if(validDay(day, month, year) && validMonth(month) && validYear(year))
+        return true;
+    else
+        return false;
 }
