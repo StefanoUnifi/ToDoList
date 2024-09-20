0b9d539e31a0b15d20a015971d20c5d74acc4605
stefa <stefano.marianelli@stud.unifi.it>
2024-09-20 15:39:30 +0200

    Removed unecessary file

--- cmake-build-debug/test/lib/googletest-main/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/stefa/CLionProjects/ToDoList/test/lib/googletest-main/googlemock
-# Build directory: C:/Users/stefa/CLionProjects/ToDoList/cmake-build-debug/test/lib/googletest-main/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- cmake-build-debug/test/lib/googletest-main/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/stefa/CLionProjects/ToDoList/test/lib/googletest-main/googletest
-# Build directory: C:/Users/stefa/CLionProjects/ToDoList/cmake-build-debug/test/lib/googletest-main/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
